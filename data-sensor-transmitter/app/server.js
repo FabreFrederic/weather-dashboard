b6751a3f289ba9e39198d147ca39b4fa23fc54eb
Frederic Fabre <fabrefrederic@free.fr>
2018-09-17 23:35:49 +0200

    data sensor transmetter automatically created with the arduino serial port

--- data-sensor-transmitter/app/server.js
+++ data-sensor-transmitter/app/server.js
@@ -1,38 +1,61 @@
-const serialport = require('./serialPort');
+'use strict';
+
+const arduinoSerialPort = require('./arduinoSerialPort');
 const EventBus = require('vertx3-eventbus-client');
 
-let sensorType;
 let dataSensor;
 let waterTemperature;
 let airTemperature;
-let airPressure;
-let airHumidity;
 
-const waterTemperatureAddress = 'water.temperature.address';
-const airTemperatureAddress = 'air.temperature.address';
-const eventBus = new EventBus('http://localhost:8080/eventbus');
+const airTemperatureAddress = 'air.temperature.raw.address';
+const waterTemperatureAddress = 'water.temperature.raw.address';
+const eventBusUrl = 'http://localhost:8080/eventbus';
+
+const waterSensorEnvironment = 'WATER';
+const airSensorEnvironment = 'AIR';
+
+const temperatureSensorType = 'TEMPERATURE';
+
+let options = {
+    // Max reconnect attempts
+    vertxbus_reconnect_attempts_max: Infinity,
+    // Initial delay (in ms) before first reconnect attempt
+    vertxbus_reconnect_delay_min: 1000,
+    // Max delay (in ms) between reconnect attempts
+    vertxbus_reconnect_delay_max: 5000,
+    // Exponential backoff factor
+    vertxbus_reconnect_exponent: 2,
+    // Randomization factor between 0 and 1
+    vertxbus_randomization_factor: 0.5
+};
+const eventBus = new EventBus(eventBusUrl, options);
+eventBus.enableReconnect(true);
 
 eventBus.onclose = (param) => {
-    console.log('closed event bus', param)
-}
+    console.log('Event bus connection closed', param)
+};
 
-eventBus.onopen = () => {
-    console.log('open event bus');
+eventBus.onopen = async () => {
+    console.log('Event bus connection is open');
+    const serialport = await arduinoSerialPort.initializeSerialPort();
+
     serialport.on('data', function (data) {
         // Identify the type of sensor
         if (data && data.length > 4) {
             dataSensor = String(data).trim();
-            sensorType = dataSensor.substring(0, 4);
+            let sensorType = dataSensor.substring(0, 4);
             let today = new Date().toISOString();
 
             switch (sensorType) {
                 case '*wt*':
                     waterTemperature = dataSensor.substring(4, dataSensor.length);
-                    sendValueToEventBus(waterTemperature, today, waterTemperatureAddress);
+                    sendValueToEventBus(waterTemperature, today, waterTemperatureAddress,
+                        waterSensorEnvironment, temperatureSensorType);
                     break;
                 case '*at*':
                     airTemperature = dataSensor.substring(4, dataSensor.length);
-                    sendValueToEventBus(airTemperature, today, airTemperatureAddress);
+                    sendValueToEventBus(airTemperature, today, airTemperatureAddress,
+                        airSensorEnvironment, temperatureSensorType);
                     break;
                 case '*ap*':
                     break;
@@ -43,17 +66,13 @@ eventBus.onopen = () => {
             }
         }
     });
+};
+
+function sendValueToEventBus(value, date, address, sensorEnvironment, sensorType) {
+    eventBus.publish(address, '{"date":"' + date +
+        '" ,"value":' + value +
+        ' ,"sensorEnvironment":' + sensorEnvironment +
+        ' ,"sensorType":' + sensorType + '}');
+
+    console.log('sendValueToEventBus - value : ' + value + ' address : ' + address);
 }
-
-function sendValueToEventBus(value, date, address) {
-    eventBus.publish(address, '{"value":' + value + ', "date":"' + date + '"}');
-    // console.log('sendValueToEventBus - value : ' + value + ' address : ' + address);
-}
-
-serialport.on('close', function (err) {
-    console.log('serial port closed', err);
-});
-
-serialport.on('open', function () {
-    console.log('serial port open');
-});
