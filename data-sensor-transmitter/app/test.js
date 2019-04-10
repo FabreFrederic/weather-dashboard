02c13e998a4716c2c23a69fece25fa31040468cd
Frederic Fabre <fabrefrederic@free.fr>
2019-04-10 23:07:24 +0200

    test node file : - add digit after the decimal point - Use a scheduler to send temperature to the backend

--- data-sensor-transmitter/app/test.js
+++ data-sensor-transmitter/app/test.js
@@ -1,27 +1,31 @@
 const EventBus = require('vertx3-eventbus-client');
+const cron = require("node-cron");
 
 const waterTemperatureAddress = 'water.temperature.address';
 const airTemperatureAddress = 'air.temperature.address';
 const eventBus = new EventBus('http://localhost:8080/eventbus');
 
 eventBus.onopen = () => {
+    cron.schedule("*/1 * * * * *", function() {
+        let waterValue = Math.floor(Math.random() * 6) + 1 + '.' + Math.floor(Math.random() * 6) + 1;
+        let airValue = Math.floor(Math.random() * 6) + 1 + '.' + Math.floor(Math.random() * 6) + 1;
+        let today = new Date().toISOString();
+
+        eventBus.publish(waterTemperatureAddress, '{"date":"' + today +
+            '" ,"value":' + waterValue +
+            ' ,"sensorEnvironment":"WATER"' +
+            ' ,"sensorType":"TEMPERATURE"}');
+
+        eventBus.publish(airTemperatureAddress, '{"date":"' + today +
+            '" ,"value":' + airValue +
+            ' ,"sensorEnvironment":"AIR"' +
+            ' ,"sensorType":"TEMPERATURE"}');
+
+        console.log('waterValue : ' + waterValue);
+        console.log('airValue : ' + airValue);
+    });
+
     console.log('open event bus');
-    let waterValue = Math.floor(Math.random() * 6) + 1;
-    let airValue = Math.floor(Math.random() * 6) + 1;
-    let today = new Date().toISOString();
-
-    eventBus.publish(waterTemperatureAddress, '{"date":"' + today +
-        '" ,"value":' + waterValue +
-        ' ,"sensorEnvironment":"WATER"' +
-        ' ,"sensorType":"TEMPERATURE"}');
-
-    eventBus.publish(airTemperatureAddress, '{"date":"' + today +
-        '" ,"value":' + airValue +
-        ' ,"sensorEnvironment":"AIR"' +
-        ' ,"sensorType":"TEMPERATURE"}');
-
-    console.log('waterValue : ' + waterValue);
-    console.log('airValue : ' + airValue);
 }
 
 eventBus.onclose = (param) => {
