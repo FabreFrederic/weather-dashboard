1d82db1e91dadfd622102199712870aa5bf2247a
Frederic Fabre <fabrefrederic@free.fr>
2019-04-11 13:30:26 +0200

    data sensor transmitter : add backend eventbus reconnection

--- data-sensor-transmitter/app/test.js
+++ data-sensor-transmitter/app/test.js
@@ -3,7 +3,22 @@ const cron = require("node-cron");
 
 const waterTemperatureAddress = 'water.temperature.address';
 const airTemperatureAddress = 'air.temperature.address';
-const eventBus = new EventBus('http://localhost:8080/eventbus');
+
+let options = {
+    // Max reconnect attempts
+    vertxbus_reconnect_attempts_max: Infinity,
+    // Initial delay (in ms) before first reconnect attempt
+    vertxbus_reconnect_delay_min: 1000,
+    // Max delay (in ms) between reconnect attempts
+    vertxbus_reconnect_delay_max: 5000,
+    // Exponential backoff factor
+    vertxbus_reconnect_exponent: 2,
+    // Randomization factor between 0 and 1
+    vertxbus_randomization_factor: 0.5
+};
+
+const eventBus = new EventBus('http://localhost:8080/eventbus', options);
+eventBus.enableReconnect(true);
 
 eventBus.onopen = () => {
     cron.schedule("*/1 * * * * *", function() {
@@ -26,8 +41,9 @@ eventBus.onopen = () => {
     });
 
     console.log('open event bus');
-}
+};
 
 eventBus.onclose = (param) => {
-    console.log('closed event bus', param)
-}
+    console.log('closed event bus', param);
+
+};
